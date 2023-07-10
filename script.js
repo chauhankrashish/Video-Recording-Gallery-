d523897a22134b8d4c7027509270c0dca560febc
Ashish-Chauhan <chauhankrashish@gmail.com>
2023-07-11 01:47:17 +0530

    All Changes posible

--- script.js
+++ script.js
@@ -41,9 +41,45 @@ recordBtnCont.addEventListener("click", (e) => {
     if (recordFlag) { //start
         recoder.start();
         recordBtn.classList.add("anime-record");
+        startTimer();
+
     }
     else { //stop
         recoder.stop();
         recordBtn.classList.remove("anime-record");
+        stopTimer()
     }
-})
+
+});
+
+let TimerID;
+let counter = 0;
+let timer = document.querySelector(".timer")
+function startTimer() {
+    timer.style.display = "block";
+    function displayTimer() {
+        let totalSecends = counter;
+
+        let hours = Number.parseInt(totalSecends / 3600);
+        totalSecends = totalSecends % 3600;
+
+        let minuts = Number.parseInt(totalSecends / 60);
+        totalSecends = totalSecends % 60;
+
+        let secends = totalSecends;
+
+
+        hours = (hours < 10) ? `0${hours}` : hours;
+        minuts = (minuts < 10) ? `0${minuts}` : minuts;
+        secends = (secends < 10) ? `0${secends}` : secends;
+
+        timer.innerText = `${hours}:${minuts}:${secends}`;
+        counter++;
+    }
+    TimerID = setInterval(displayTimer, 1000);
+}
+function stopTimer() {
+    clearInterval(TimerID);
+    timer.style.display = "none";
+    timer.innerText = "00:00:00";
+}
